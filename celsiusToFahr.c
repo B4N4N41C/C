27f72623eec4280fbab0d09980432a92a356ff87
Nikolay Mochalin <mochalin.nikolay2017@yandex.ru>
2024-08-17 21:45:53 +0300

    celsiusToFahr.c

---
+++ celsiusToFahr.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main()
+{
+		float fahr, celsius;
+		int lower, upper, step;
+
+		lower = -20; /* нижняя граница таблицы температур */
+		upper = 20; /* верхняя граница */
+		step = 1; /* шаг */
+
+		celsius = lower;
+		printf ("fahr\tcelsius \n");
+		while (celsius <= upper) {
+				fahr = ((9.0/5.0) * celsius) + 32.0;
+				printf ("%3.1f\t%6.1f\n", fahr, celsius);
+				celsius = celsius + step;
+		}
+		return 0;
+}
