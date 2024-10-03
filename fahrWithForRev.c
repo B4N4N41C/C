d31285af5276548d3720579a0618ad8de44527eb
Nikolay Mochalin <mochalin.nikolay2017@yandex.ru>
2024-10-03 21:51:36 +0300

    output.c

--- fahrWithForRev.c
+++ fahrWithForRev.c
@@ -1,11 +1,14 @@
 #include <stdio.h>
 
-/* печать таблицы температур по Фаренгейту и Цульсию */
+/* печать таблицы температур по Фаренгейту и Цульсию в обратном порядке*/
+#define LOWER 0 /* нижния граница таблицы */
+#define UPPER 300 /* верхняя граница */
+#define STEP 20 /* размер шага */
 
 int main()
 {
 	int fahr;
-	for (fahr = 0; fahr <= 300; fahr = fahr + 20)
+	for (fahr = LOWER; fahr <= UPPER; fahr = fahr + STEP)
 			printf ("%3d %6.1f\n", fahr, (5.0/9.0)*(fahr-32));
 	return 0;
 }	
---
+++ output.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/* копирование ввода на вывод; 1-я версия */
+int main()
+{
+	int c;
+	c = getchar();
+	while (c != EOF) {
+		putchar(c);
+		c = getchar();
+	}
+	return 0;
+}
